6747da60c132a4f6b839c7d507560fb09ddac99c
DevHieu <hieudd2090@gmail.com>
2022-01-30 13:42:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import moment from "moment";
+import { useState, useEffect } from "react";
+import {
+  BsCloudSun,
+  BsCloudy,
+  BsCloudRain,
+  BsCloudSnow,
+  BsSun,
+} from "react-icons/bs";
+import style from "./App.module.scss";
 
 function App() {
+  // const [lat, setLat] = useState([]);
+  // const [long, setLong] = useState([]);
+  const [city, setCity] = useState("");
+  const [data, setData] = useState([]);
+  const [temp, setTemp] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [speed, setSpeed] = useState("");
+  const [sunrise, setSunrise] = useState("");
+  const [sunset, setSunset] = useState("");
+  const [weather, setWeather] = useState("");
+  const [icon, setIcon] = useState(<BsSun />);
+  const [query, setQuery] = useState("Long Thành");
+
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather/";
+  const API_KEY = "b96d7c571ab235ab040c28e2be03e6f4";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // navigator.geolocation.getCurrentPosition((position) => {
+      //   setLat(position.coords.latitude);
+      //   setLong(position.coords.longitude);
+      // });
+
+      await fetch(`${API_URL}?q=${query}&appid=${API_KEY}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          setTemp(Math.round(Math.round(result.main.temp) / 10));
+          setWeather(result.weather[0].main);
+          setHumidity(result.main.humidity);
+          setSpeed(result.wind.speed);
+          setSunrise(
+            new Date(data.sys.sunrise * 1000).toLocaleTimeString("en-IN")
+          );
+          setSunset(
+            new Date(data.sys.sunset * 1000).toLocaleTimeString("en-IN")
+          );
+          setCity("");
+        });
+    };
+    fetchData();
+
+    // ${API_URL}?q=${query}&limit=5&appid=${API_KEY}
+
+    switch (weather) {
+      case "Clear":
+        setIcon(BsSun);
+        break;
+      case "Clouds":
+        setIcon(BsCloudy);
+        break;
+      case "CloudSun":
+        setIcon(BsCloudSun);
+        break;
+      case "Rain":
+        setIcon(BsCloudRain);
+        break;
+      case "Snow":
+        setIcon(BsCloudSnow);
+        break;
+      default:
+        setIcon(BsCloudSun);
+    }
+  }, [query, weather]);
+
+  const enterPress = (evn) => {
+    if (evn.key === "Enter") {
+      setQuery(city);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.app}>
+      <div className={style.wrapper}>
+        <div className={style.header}>
+          <input
+            placeholder="Search Your City..."
+            onChange={(value) => setCity(value.target.value)}
+            value={city}
+            onKeyPress={enterPress}
+            className={style.inputBox}
+          />
+          <div className={style.headerBody}>
+            <div className={style.headerLeft}>
+              <h1 className={style.temp}>{temp}&#8451;</h1>
+              <h2 className={style.weatherIcon}>{icon}</h2>
+              <h2 className={style.weather}>{weather}</h2>
+              <p>{moment().format("dddd, MMMM Do YYYY")}</p>
+            </div>
+          </div>
+          <div className={style.headerRight}>
+            <h1 className={style.city}>
+              {data.name ? data.name : "Your city is undefined"}
+            </h1>
+          </div>
+        </div>
+        <div className={style.footer}>
+          <div>
+            <p className={style.footerItem}>Humidity: {humidity} %</p>
+            <p>Wind: {speed}Km/h</p>
+          </div>
+          <div>
+            <p className={style.footerItem}>
+              Sunrise:
+              {sunrise}
+            </p>
+            <p>
+              Sunset:
+              {sunset}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
